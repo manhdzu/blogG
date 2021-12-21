69ebb407b4292517aa4de0ddc4b5c079dd46b453
manhdzu <vunguyenducmanh10a@gmail.com>
2021-12-21 13:11:44 +0700

    Done

--- client/src/components/posts/Post.js
+++ client/src/components/posts/Post.js
@@ -7,9 +7,9 @@ import "./post.scss";
 const Post = ({ post }) => {
    const postDate = getFormattedDate(post.date);
    return (
-      <Card className="deckStyle" style={{ border: "none"}}>
-         <Card.Body className="postCover">
-            <Card.Title className="text-center p-5">{post.title}</Card.Title>
+      <Card className="deckStyle" style={{ border: "none", height: "100%"}}>
+         <Card.Body className="postCover" bg="secondary">
+            <Card.Title className="text-center p-5" >{post.title}</Card.Title>
          </Card.Body>
          <Card.Footer>
             <small className="text-muted">Posted on: {postDate}, by: {post.author}</small>
