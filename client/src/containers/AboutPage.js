f5e89a5e6f094ae91747f102a09338211ed22e1c
manhdzu <88848672+manhdzu@users.noreply.github.com>
2021-12-22 18:27:53 +0700

    Update AboutPage.js

--- client/src/containers/AboutPage.js
+++ client/src/containers/AboutPage.js
@@ -12,9 +12,9 @@ const About = () => {
             <Row><h6>Faculty of Electrical and Electronics Engineering</h6></Row>
             <Row className="my-2"><img src={HCMUTE} alt=""></img></Row>
             <Row><h3>PROJECT 2</h3></Row>
-            <Row><h2>BLOG WEBSITE USING MERN APP</h2></Row>
+            <Row><h2>BLOG WEB APPLICATION USING MERN APP</h2></Row>
             <Row><h6 className="mt-3">Doer: VŨ NGUYỄN ĐỨC MẠNH</h6></Row>
-            <Row><h6 className="mt-2">Guiding Teacher: PhD. TRƯƠNG NGỌC HÀ</h6></Row>
+            <Row><h6 className="mt-2">Guiding Teacher: MSc. TRƯƠNG NGỌC HÀ</h6></Row>
             <Row className='mt-4'><Link to="/more"><h6>{"More information >"}</h6></Link></Row>
 		</Container>
 	)
